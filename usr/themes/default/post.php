7504833bde88b1abf48b4293f89e7633e9805b58
valleykid <valleykiddy@gmail.com>
2019-10-30 21:06:05 +0800

    feat: add copyright

--- usr/themes/default/post.php
+++ usr/themes/default/post.php
@@ -13,6 +13,23 @@
             <?php $this->content(); ?>
         </div>
         <p itemprop="keywords" class="tags"><?php _e('标签: '); ?><?php $this->tags(', ', true, 'none'); ?></p>
+        <!--<a href="http://creativecommons.org/licenses/by-nc-sa/3.0/cn/" rel="external nofollow" target="_blank">CC BY-NC-SA 3.0 CN</a> 许可协议-->
+        <div>
+            <ul class="post-copyright">
+                <li class="post-copyright-author">
+                    <strong>本文作者：</strong>
+                    <a href="<?php $this->author->permalink(); ?>">vk</a>
+                </li>
+                <li class="post-copyright-link">
+                    <strong>本文链接：</strong>
+                    <a href="<?php $this->permalink() ?>" title="<?php $this->title() ?>"><?php $this->permalink() ?></a>
+                </li>
+                <li class="post-copyright-license">
+                    <strong>版权声明： </strong>
+                    本博客所有文章除特别声明外，均采用 <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank" rel="external nofollow">知识共享署名4.0</a> 国际许可协议进行许可。转载请注明出处！
+                </li>
+            </ul>
+        </div>
     </article>
 
     <?php $this->need('comments.php'); ?>
